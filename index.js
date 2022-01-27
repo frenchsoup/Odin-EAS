985d97feda34dc6865e6673c63f59b97d08f8bcb
Alex LeFevre <alexander.m.lefevre@gmail.com>
2022-01-26 19:12:17 -0500

    working etch a sketch

--- index.js
+++ index.js
@@ -1,23 +1,68 @@
-let gridDiv = document.getElementsByClassName('grid');
+// gets the grid container
+let gridContainer = document.getElementById('gridContainer');
+// gets all divs with class grid
+let gridDivs = document.getElementsByClassName('grid');
+// creates 1 div with class grid
+let grid = document.createElement('div');
+// default grid size
+let gridSize = 16;
 
 /**
- * loop to create grid divs
+ * loop to create grid divs for default on page load
  */
-for (x = 0; x < 256; x++) {
+for (x = 0; x < gridSize * gridSize; x++) {
   let grid = document.createElement('div');
-  grid.style.height = '35px';
-  grid.style.width = '35px';
+  grid.style.height = 560 / gridSize + 'px';
+  grid.style.width = 560 / gridSize + 'px';
   grid.className = 'grid';
   grid.id = 'grid';
   document.getElementById('gridContainer').appendChild(grid);
+}
+etch();
+
+/**
+ * Method to prompt user for box size and create new grid
+ */
+function resizeGrid() {
+  gridContainer.innerHTML = '';
+  let promptInput = prompt(
+    'Please enter new grid size. (Just height or width)',
+    '10'
+  );
+  let promptSize = parseInt(promptInput);
+  if (promptSize < 100) {
+    let newGridArea = promptSize * promptSize;
+    for (x = 0; x < newGridArea; x++) {
+      let grid = document.createElement('div');
+      grid.style.height = 560 / promptSize + 'px';
+      grid.style.width = 560 / promptSize + 'px';
+      grid.className = 'grid';
+      grid.id = 'grid';
+      document.getElementById('gridContainer').appendChild(grid);
+    }
+    etch();
+  } else {
+    alert('Number must be less than 100');
+  }
 }
 
 /**
  * loop to add event listener and function to all gridDivs
  * function changes background
  */
-for (let i = 0; i < gridDiv.length; i++) {
-  gridDiv[i].addEventListener('mouseover', function (e) {
-    e.target.style.backgroundColor = 'black';
-  });
+function etch() {
+  for (let i = 0; i < gridDivs.length; i++) {
+    gridDivs[i].addEventListener('mouseover', function (e) {
+      e.target.style.backgroundColor = 'black';
+    });
+  }
+}
+/**
+ * Method to clear grid
+ */
+function clearGrid() {
+  for (let i = 0; i < gridDivs.length; i++) {
+    gridDivs[i].style.backgroundColor = 'white';
+  }
+  resizeGrid();
 }
